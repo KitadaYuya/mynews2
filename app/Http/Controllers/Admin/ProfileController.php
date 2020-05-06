64205eb924c00e689f69f7ef188a23a68cbb181b
北田裕也 <kitadayuya@gmail.com>
2020-05-06 02:34:02 +0000

    課題17

--- app/Http/Controllers/Admin/ProfileController.php
+++ app/Http/Controllers/Admin/ProfileController.php
@@ -5,6 +5,8 @@ namespace App\Http\Controllers\Admin;
 use Illuminate\Http\Request;
 use App\Http\Controllers\Controller;
 use App\Profile;
+use App\ProfileHistory;
+use Carbon\Carbon;
 
 class ProfileController extends Controller
 {
@@ -30,7 +32,6 @@ class ProfileController extends Controller
 
     public function edit(Request $request)
     {
-        \Log::debug(__LINE__. ' '.__FILE__);
       $profile = Profile::find($request->id);
       if (empty($profile)) {
         abort(404);    
@@ -50,6 +51,12 @@ class ProfileController extends Controller
 
       // 該当するデータを上書きして保存する
       $profile->fill($profile_form)->save();
+      
+      $history = new ProfileHistory;
+        $history->profile_id = $profile->id;
+        $history->edited_at = Carbon::now();
+        $history->save();
+
 
       return redirect('admin/profile/edit');
     }
